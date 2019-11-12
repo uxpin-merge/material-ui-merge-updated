92aaeee77a3ac61a3f9746eb380a48ba43f8c648
Jack Behar <jbehar@taylordigital.io>
2019-11-12 14:44:26 -0500

    Updates

--- src/DeviceViewer/presets/0-default.jsx
+++ src/DeviceViewer/presets/0-default.jsx
@@ -2,5 +2,7 @@ import * as React from 'react';
 import DeviceViewer from '../DeviceViewer';
 
 export default (
-   <DeviceViewer UXPin="1"/>
+   <DeviceViewer UXPin="1">
+      Drop children here
+   </DeviceViewer>
 );
