e4420c897c5bf4777a6bfdd56ff41e7f95e10f73
Jack Behar <jbehar@taylordigital.io>
2019-11-19 16:55:39 -0500

    updates

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,12 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import BoxM from '@material-ui/core/Box';
-const theme = {
-  spacing: [0, 2, 3, 5, 8],
-}
+
+
 function Box(props) {
-   return (
-     <BoxM {...props} paddingTop={props.paddingTop} paddingRight={props.paddingRight} paddingBottom={props.paddingBottom} paddingLeft={props.paddingLeft}>
+
+  let pt = props.paddingTop * 8;
+  let pr = props.paddingRight * 8;
+  let pb = props.paddingBottom * 8;
+  let pl = props.paddingLeft * 8;
+  let mt = props.marginTop * 8;
+  let mr = props.marginRight * 8;
+  let mb = props.marginBottom * 8;
+  let ml = props.marginLeft * 8;
+  
+return (
+     <BoxM {...props}
+      style={{
+        paddingTop: pt,
+        paddingRight: pr,
+        paddingBottom: pb,
+        paddingLeft: pl,
+        marginTop: mt,
+        marginRight: mr,
+        marginBottom: mb,
+        marginLeft: ml,
+      }} >
        {props.children}
      </BoxM>
    );
@@ -21,30 +40,54 @@ Box.propTypes = {
   * Each unit equals 8dp. Example:
   * 0=px, 1=8px, 2=16px, 3=24px ... 
   */
-  paddingTop: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
+  paddingTop: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   /**
   * Right Padding.
   * Each unit equals 8dp. Example:
   * 0=px, 1=8px, 2=16px, 3=24px ... 
   */
-  paddingRight: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
+  paddingRight: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   /**
   * Bottom Padding.
   * Each unit equals 8dp. Example:
   * 0=px, 1=8px, 2=16px, 3=24px ... 
   */
-  paddingBottom: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
+  paddingBottom: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
   /**
   * Left Padding.
   * Each unit equals 8dp. Example:
   * 0=px, 1=8px, 2=16px, 3=24px ... 
   */
-  paddingLeft: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6]),
-  // component: React.ElementType
-  // clone: PropTypes.bool,
- };
- Box.defaultProps = {
-  // paddingLeft: 0,
+  paddingLeft: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
 
-};
+
+    /**
+  * Top margin.
+  * Each unit equals 8dp. Example:
+  * 0=px, 1=8px, 2=16px, 3=24px ... 
+  */
+ marginTop: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+ /**
+ * Right margin.
+ * Each unit equals 8dp. Example:
+ * 0=px, 1=8px, 2=16px, 3=24px ... 
+ */
+marginRight: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+ /**
+ * Bottom margin.
+ * Each unit equals 8dp. Example:
+ * 0=px, 1=8px, 2=16px, 3=24px ... 
+ */
+marginBottom: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+ /**
+ * Left margin.
+ * Each unit equals 8dp. Example:
+ * 0=px, 1=8px, 2=16px, 3=24px ... 
+ */
+ marginLeft: PropTypes.oneOf([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+
+
+
+ };
+
  export { Box as default };
--- src/Box/presets/0-default.jsx
+++ src/Box/presets/0-default.jsx
@@ -3,7 +3,17 @@ import * as React from "react";
 import Box from "../Box";
 
 export default (
-  <Box uxpId="box1" paddingTop={0} paddingRight={0} paddingBottom={0} paddingLeft={0}>
+  <Box
+    uxpId="box1"
+    paddingTop={0}
+    paddingRight={0}
+    paddingBottom={0}
+    paddingLeft={0}
+    marginTop={0}
+    marginRight={0}
+    marginBottom={0}
+    marginLeft={0}
+  >
     Box
   </Box>
 );
