034972adabf3a790a6793ee83bd4173e57326ed9
Jack Behar <jbehar@taylordigital.io>
2020-01-02 16:40:59 -0500

    Clean-up

--- src/DialogUXP/presets/0-default.jsx
+++ src/DialogUXP/presets/0-default.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import Button from "../../Button/Button";
+import Button from "../../ButtonUXP/ButtonUXP";
 import DialogActions from "../../DialogActions/DialogActions"
 import DialogContent from "../../DialogContent/DialogContent";
 import DialogContentText from "../../DialogContentText/DialogContentText";
@@ -8,7 +8,7 @@ import DialogTitle from "../../DialogTitle/DialogTitle";
 import DialogUXP from "../DialogUXP";
 
 export default (
-  <DialogUXP open={true} height="400" uxpId="1">
+  <DialogUXP open="true" height="400" uxpId="1">
     <DialogTitle uxpId="2">Dialog Title</DialogTitle>
     <DialogContent uxpId="3">
       <DialogContentText uxpId="4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eleifend viverra urna non lacinia. Curabitur sed euismod tortor. Vestibulum ultricies enim at quam finibus ultricies ut non lorem.</DialogContentText>
--- uxpin.config.js
+++ uxpin.config.js
@@ -1,202 +1,131 @@
 module.exports = {
   components: {
     categories: [
-{
-  name: "Layout",
-  include:[
-    "src/Box/Box.js",
-    "src/Grid/Grid.js",
-    "src/GridList/GridList.js",
-    "src/Hidden/Hidden.js",
-  ]
-},
-{
-  name: "Inputs",
-  include:[
-    "src/ButtonUXP/ButtonUXP.js",
-    "src/Checkbox/Checkbox.js",
-    "src/FabRoundUXP/FabRoundUXP.js",
-    "src/FabExtendedUXP/FabExtendedUXP.js",
-    "src/Radio/Radio.js",
-    "src/RadioGroup/RadioGroup.js",
-    "src/SelectUXP/SelectUXP.js",
-    "src/TextField/TextField.js",
-
-
-  ]
-},
-
-{
-  name: "Parts",
-  include:[
-    "src/GridListTile/GridListTile.js",
-    "src/GridListTileBar/GridListTileBar.js"
-  ]
-},
-
-
-
       {
-        name: "UXPin Components",
+        name: "Layout",
         include: [
-          "src/SelectIgloo/SelectIgloo.js",
-          "src/MenuNew/MenuNew.js",
-          "src/ClickAwayListener/ClickAwayListener.js",
-          // "src/Hidden/Hidden.js",
-          "src/IglooSimpleLayoutContent/IglooSimpleLayoutContent.js",
-          "src/IglooSimpleLayout/IglooSimpleLayout.js",
-          "src/TestComponent/TestComponent.js",
-          "src/DeviceViewer/DeviceViewer.js",
-          // "src/ButtonStyled/ButtonStyled.js",
-          "src/IglooHeader/IglooHeader.js",
-          "src/IglooSmallHeader/IglooSmallHeader.js",
-          "src/IglooLargeHeader/IglooLargeHeader.js",
-          "src/IglooSideNavigation/IglooSideNavigation.js",
-          "src/IglooTopNavigation/IglooTopNavigation.js",
-          "src/ComponentDemoUXP/ComponentDemoUXP.js",
-          "src/IconUXP/IconUXP.js",
-          // "src/Box/Box.js",
-          // "src/FabRoundUXP/FabRoundUXP.js",
-          // "src/FabExtendedUXP/FabExtendedUXP.js",
-          // "src/ButtonUXP/ButtonUXP.js",
-          "src/AvatarLetterUXP/AvatarLetterUXP.js",
-          "src/AvatarImageUXP/AvatarImageUXP.js",
-          "src/AvatarIconUXP/AvatarIconUXP.js",
-          "src/DividerUXP/DividerUXP.js",
-          // "src/SliderUXP/SliderUXP.js",
-          "src/TableUXP/TableUXP.js",
-          "src/ThemeTarget/ThemeTarget.js",
-          "src/DrawerModalUXP/DrawerModalUXP.js",
-          "src/DialogUXP/DialogUXP.js",
-          "src/DialogTitle/DialogTitle.js",
-          "src/DialogContentText/DialogContentText.js",
-          "src/DialogContent/DialogContent.js",
-          "src/DialogActions/DialogActions.js",
-          "src/DialogSimpleUXP/DialogSimpleUXP.js",
-          "src/TooltipUXP/TooltipUXP.js",
-          "src/Banner/Banner.js",
-          "src/MenuUXP/MenuUXP.js",
-          // "src/MenuExposedUXP/MenuExposedUXP.js",
-          "src/ComponentInspectorUXP/ComponentInspectorUXP.js"
+          "src/Box/Box.js",
+          "src/Grid/Grid.js",
+          "src/GridList/GridList.js",
+          "src/Hidden/Hidden.js"
         ]
       },
       {
-        name: "General",
-        include: ["src/Icon/Icon.js",
-        "src/Typography/Typography.js"]
-      },
-      {
-        name: "Actions",
+        name: "Inputs",
         include: [
-          // "src/Button/Button.js",
+          "src/ButtonUXP/ButtonUXP.js",
           "src/IconButton/IconButton.js",
-          "src/Fab/Fab.js",
-          "src/Link/Link.js"
+          "src/Checkbox/Checkbox.js",
+          "src/FabRoundUXP/FabRoundUXP.js",
+          "src/FabExtendedUXP/FabExtendedUXP.js",
+          "src/Radio/Radio.js",
+          "src/RadioGroup/RadioGroup.js",
+          "src/SelectUXP/SelectUXP.js",
+          "src/TextField/TextField.js",
+          "src/Switch/Switch.js"
         ]
       },
       {
         name: "Navigation",
         include: [
-          "src/AppBar/AppBar.js",
-          "src/Toolbar/Toolbar.js",
           "src/BottomNavigation/BottomNavigation.js",
-          "src/BottomNavigationAction/BottomNavigationAction.js",
-          "src/Tab/Tab.js",
-          "src/Tabs/Tabs.js",
-          "src/TabsContainer/TabsContainer.js",
-          "src/TabsContainer/TabContent/TabContent.js",
-          "src/Drawer/Drawer.js"
+          "src/DrawerModalUXP/DrawerModalUXP.js",
+          "src/Link/Link.js",
+          "src/MenuNew/MenuNew.js",
+          "src/Tabs/Tabs.js"
         ]
       },
-      // {
-      //   name: "Layout",
-      //   include: [
-      //     "src/Grid/Grid.js",
-      //     "src/GridList/GridList.js",
-      //     "src/GridListTile/GridListTile.js",
-      //     "src/GridListTileBar/GridListTileBar.js"
-      //   ]
-      // },
+
       {
-        name: "Card",
+        name: "Surfaces",
         include: [
+          "src/IglooHeader/IglooHeader.js",
+          "src/Paper/Paper.js",
           "src/Card/Card.js",
+          "src/Collapse/Collapse.js"
+        ]
+      },
+
+      {
+        name: "Feedback",
+        include: [
+          "src/CircularProgress/CircularProgress.js",
+          "src/LinearProgress/LinearProgress.js",
+          "src/DialogUXP/DialogUXP.js",
+          "src/Banner/Banner.js"
+        ]
+      },
+      {
+        name: "Data Display",
+        include: [
+          "src/Avatar/Avatar.js",
+          "src/Badge/Badge.js",
+          "src/Chip/Chip.js",
+          "src/AvatarLetterUXP/AvatarLetterUXP.js",
+          "src/AvatarImageUXP/AvatarImageUXP.js",
+          "src/AvatarIconUXP/AvatarIconUXP.js",
+          "src/DividerUXP/DividerUXP.js",
+          "src/Icon/Icon.js",
+          "src/Image/Image.js",
+          "src/List/List.js",
+          "src/TableUXP/TableUXP.js",
+          "src/TooltipUXP/TooltipUXP.js",
+          "src/Typography/Typography.js"
+        ]
+      },
+      {
+        name: "Parts",
+        include: [
+          "src/GridListTile/GridListTile.js",
+          "src/GridListTileBar/GridListTileBar.js",
+          "src/IglooSideNavigation/IglooSideNavigation.js",
+          "src/IglooTopNavigation/IglooTopNavigation.js",
           "src/CardActions/CardActions.js",
           "src/CardContent/CardContent.js",
           "src/CardHeader/CardHeader.js",
-          "src/CardMedia/CardMedia.js"
-        ]
-      },
-      {
-        name: "Form",
-        include: [
-          // "src/Checkbox/Checkbox.js",
-          // "src/Radio/Radio.js",
-          "src/Switch/Switch.js",
-          // "src/TextField/TextField.js",
-          "src/Input/Input.js",
-          "src/Select/Select.js",
-          // "src/RadioGroup/RadioGroup.js",
-          "src/Menu/Menu.js",
-          "src/MenuItem/MenuItem.js",
-          "src/MenuList/MenuList.js",
-          "src/OutlinedInput/OutlinedInput.js",
-          "src/FilledInput/FilledInput.js",
-          "src/InputLabel/InputLabel.js",
-          "src/FormControl/FormControl.js",
-          "src/FormHelperText/FormHelperText.js",
-          "src/FormGroup/FormGroup.js",
-          "src/FormControlLabel/FormControlLabel.js",
-          "src/FormLabel/FormLabel.js",
-          // "src/RadioGroup/RadioGroup.js"
-        ]
-      },
-      {
-        name: "Content",
-        include: [
-          "src/Paper/Paper.js",
-          "src/Collapse/Collapse.js",
-          "src/Avatar/Avatar.js",
-          "src/Badge/Badge.js",
-          "src/Backdrop/Backdrop.js",
-          "src/Chip/Chip.js"
-        ]
-      },
-      {
-        name: "List",
-        include: [
-          "src/List/List.js",
+          "src/CardMedia/CardMedia.js",
+          "src/DialogTitle/DialogTitle.js",
+          "src/DialogContentText/DialogContentText.js",
+          "src/DialogContent/DialogContent.js",
+          "src/DialogActions/DialogActions.js",
           "src/ListItem/ListItem.js",
           "src/ListItemAvatar/ListItemAvatar.js",
           "src/ListItemIcon/ListItemIcon.js",
           "src/ListItemSecondaryAction/ListItemSecondaryAction.js",
           "src/ListItemText/ListItemText.js",
-          "src/ListSubheader/ListSubheader.js"
+          "src/ListSubheader/ListSubheader.js",
+          "src/IglooSimpleLayoutContent/IglooSimpleLayoutContent.js",
+          "src/BottomNavigationAction/BottomNavigationAction.js",
+          "src/Tab/Tab.js",
+          "src/Toolbar/Toolbar.js",
+          "src/AppBar/AppBar.js",
+          "src/MenuItem/MenuItem.js",
+          "src/MenuList/MenuList.js",
+          "src/InputLabel/InputLabel.js",
+          "src/FormControl/FormControl.js",
+          "src/FormHelperText/FormHelperText.js",
+          "src/FormGroup/FormGroup.js",
+          "src/FormControlLabel/FormControlLabel.js",
+          "src/FormLabel/FormLabel.js"
         ]
       },
       {
-        name: "Progress",
+        name: "Templates",
         include: [
-          "src/CircularProgress/CircularProgress.js",
-          "src/LinearProgress/LinearProgress.js"
+          "src/IglooSimpleLayout/IglooSimpleLayout.js",
+          "src/ComponentDemoUXP/ComponentDemoUXP.js"
         ]
       },
       {
-        name: "HTML Wrappers and Spacing",
-        include: ["src/Image/Image.js", "src/CssBaseline/CssBaseline.js"]
+        name: "Misc",
+        include: [
+          "src/DeviceViewer/DeviceViewer.js",
+          "src/TestComponent/TestComponent.js",
+          "src/ThemeTarget/ThemeTarget.js",
+          "src/ComponentInspectorUXP/ComponentInspectorUXP.js"
+        ]
       }
     ]
   },
-  name: "Material Design UI"
+  name: "IGLOO DESIGN SYSTEM"
 };
-
-/**
- * Currently not supported:
- * 'src/Menu/Menu.js',
- * 'src/ExpansionPanel/ExpansionPanel.js',
- * 'src/ExpansionPanelActions/ExpansionPanelActions.js',
- * 'src/ExpansionPanelDetails/ExpansionPanelDetails.js',
- * 'src/ExpansionPanelSummary/ExpansionPanelSummary.js'
- * 'src/Snackbar/Snackbar.js'
- * 'src/SnackbarContent/SnackbarContent.js'
- */
