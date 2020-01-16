9cd9b58f29ad978872fbd4ac2295d1c7f722bd61
Jack Behar <jbehar@taylordigital.io>
2020-01-16 15:37:37 -0500

    Added chip

--- src/Chip/Chip.js
+++ src/Chip/Chip.js
@@ -1,75 +1,123 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { makeStyles } from '@material-ui/core/styles';
 import ChipM from "@material-ui/core/Chip";
+import Icon from "../Icon/Icon"
+
+const useStyles = makeStyles(theme => ({
+  chipMargin: {
+    margin: theme.spacing(0.5),
+  },
+}));
 
 function Chip(props) {
-  return <ChipM {...props} />;
+  const classes = useStyles();
+  function handleDelete() {
+    alert("You clicked the delete icon.");
+  }
+
+  const [chipDeleted, setChipDeleted] = React.useState(false);
+
+  return (
+    <>
+      {!chipDeleted ? (
+        <ChipM
+          {...props}
+          onDelete={
+            props.deletable
+              ? () => {
+                  setChipDeleted(true);
+                }
+              : null
+          }
+          icon={
+            props.icon ? 
+            <Icon>{props.icon}</Icon>
+            : null
+          }
+          className={ props.hasMargins ? classes.chipMargin : null }
+        />
+      ) : null}
+    </>
+  );
 }
 
 Chip.propTypes = {
   /**
+   * If set, icon will display. Use the name of the icon from https://material.io/tools/icons.
+   */
+  icon: PropTypes.string,
+
+  /**
+   * The content of the label.
+   */
+  label: PropTypes.string,
+
+  /**
+   * If true, the chip will appear clickable.
+   */
+  clickable: PropTypes.bool,
+
+  /**
+   * If true, the chip will appear show delete icon.
+   */
+  deletable: PropTypes.bool,
+
+  /**
+  * If true, the chip will have a margin. This is helpful when creating a collection of chips.
+  */
+  hasMargins: PropTypes.bool,
+  
+  /**
+   * Click event to use with UXPin interactions.
+   */
+  onClick: PropTypes.func,
+  
+
+  /**
+   * UNUSED PROPS
+   */
+
+   /**
+   * The color of the component. It supports those theme colors that make sense for this component.
+   * @uxpinignoreprop
+   */
+  color: PropTypes.oneOf(["default", "primary", "secondary"]),
+
+  /**
+   * Delete event to use with UXPin interactions.
+   * @uxpinignoreprop
+   */
+  onDelete: PropTypes.func,
+
+  /**
    * Avatar element.
+   * @uxpinignoreprop
    */
   avatar: PropTypes.element,
 
   /**
    * Override or extend the styles applied to the component.
+   * @uxpinignoreprop
    */
   classes: PropTypes.object,
 
   /**
-   * If true, the chip will appear clickable, and will raise when pressed,
-   * even if the onClick property is not defined.
-   * If false, the chip will not be clickable, even if onClick property is defined.
-   * This can be used, for example,
-   * along with the component property to indicate an anchor Chip is clickable.
-   */
-  clickable: PropTypes.bool,
-
-  /**
-   * The color of the component. It supports those theme colors that make sense for this component.
-   */
-  color: PropTypes.oneOf(["default", "primary", "secondary"]),
-
-  /**
    * The component used for the root node.
    * Either a string to use a DOM element or a component.
+   * @uxpinignoreprop
    */
   component: PropTypes.string,
 
   /**
    * Override the default delete icon element. Shown only if `onDelete` is set.
+   * @uxpinignoreprop
    */
   deleteIcon: PropTypes.node,
 
   /**
-   * Icon element.
-   */
-  icon: PropTypes.node,
-
-  /**
-   * The content of the label.
-   */
-  label: PropTypes.node,
-  onClick: PropTypes.func,
-
-  /**
-   * Callback function fired when the delete icon is clicked.
-   * If set, the delete icon will be shown.
-   */
-  onDelete: PropTypes.func,
-
-  onKeyDown: PropTypes.func,
-
-  onKeyUp: PropTypes.func,
-
-  tabIndex: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string
-  ]),
-
-  /**
    * The variant to use.
+   * @uxpinignoreprop
    */
   variant: PropTypes.oneOf(["default", "outlined"])
 };
--- src/Chip/presets/0-default.jsx
+++ src/Chip/presets/0-default.jsx
@@ -1,11 +1,6 @@
 import * as React from "react";
 import Chip from "../Chip";
-import Avatar from "../../Avatar/Avatar";
-import Icon from "../../Icon/Icon";
 
 export default (
-  <Chip
-    uxpId="1"
-    avatar={<Avatar uxpId="1.1"><Icon uxpId="1.1.1">face</Icon></Avatar>}
-    label="Clickable Deletable Chip" />
+  <Chip label="I'm Chip" uxpId="1"/>
 );
