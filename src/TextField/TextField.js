7eab9e93e6e3e4b0927407ac332547ca5eeb578a
Jack Behar <jbehar@taylordigital.io>
2020-01-16 20:29:14 -0500

    update

--- src/TextField/TextField.js
+++ src/TextField/TextField.js
@@ -1,176 +1,227 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { makeStyles } from '@material-ui/core/styles';
 import TextFieldM from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import Icon from "@material-ui/core/Icon";
 
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  },
+}));
 function TextField(props) {
-  return <TextFieldM {...props} />;
+  const classes = useStyles();
+  return <TextFieldM margin="dense" {...props}
+  InputProps={{
+    startAdornment: (
+      props.icon ?
+      <InputAdornment position="start">
+        <Icon>{props.icon}</Icon>
+      </InputAdornment>
+      : null
+    ),
+  }}
+  className={classes.textField}/>;
 }
 
 TextField.propTypes = {
   /**
-   * This property helps users to fill forms faster, especially on mobile devices.
-   * The name can be confusing, as it's more like an autofill.
-   * You can learn more about it here:
-   * https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill
+   * The label text.
    */
-  autoComplete: PropTypes.string,
+  label: PropTypes.string,
 
   /**
-   * If `true`, the input will be focused during the first mount.
+   * The default value of the textfield.
    */
-  autoFocus: PropTypes.bool,
-
-  children: PropTypes.node,
-
-  className: PropTypes.string,
+  defaultValue: PropTypes.string,
 
   /**
-   * The default value of the `Input` element.
+   * The helper text.
    */
-  defaultValue: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
-
-  /**
-   * If `true`, the input will be disabled.
-   */
-  disabled: PropTypes.bool,
-
-  /**
-   * If `true`, the label will be displayed in an error state.
-   */
-  error: PropTypes.bool,
-
-  FormHelperTextProps: PropTypes.object,
-
-  /**
-   * If `true`, the input will take up the full width of its container.
-   */
-  fullWidth: PropTypes.bool,
-
-  /**
-   * The helper text content.
-   */
-  helperText: PropTypes.node,
-
-  /**
-   * The id of the `input` element.
-   * Use that property to make `label` and `helperText` accessible for screen readers.
-   */
-  id: PropTypes.string,
-
-  InputLabelProps: PropTypes.object,
-
-  /**
-   * Properties applied to the `Input` element.
-   */
-  InputProps: PropTypes.object,
-
-  /**
-   * Attributes applied to the native `input` element.
-   */
-  inputProps: PropTypes.object,
-
-  /**
-   * Use that property to pass a ref callback to the native input component.
-   */
-  inputRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.object
-  ]),
-
-  /**
-   * The label content.
-   */
-  label: PropTypes.node,
-
-  /**
-   * If `dense` or `normal`, will adjust vertical spacing of this and contained components.
-   */
-  margin: PropTypes.oneOf(["none", "dense", "normal"]),
-
-  /**
-   * If `true`, a textarea element will be rendered instead of an input.
-   */
-  multiline: PropTypes.bool,
-
-  /**
-   * Name attribute of the `input` element.
-   */
-  name: PropTypes.string,
-
-  onBlur: PropTypes.func,
-
-  /**
-   * Callback fired when the value is changed.
-   */
-  onChange: PropTypes.func,
-
-  onFocus: PropTypes.func,
+  helperText: PropTypes.string,
 
   /**
    * The short hint displayed in the input before the user enters a value.
    */
   placeholder: PropTypes.string,
 
+    /**
+   * If set, icon will display. Use the name of the icon from https://material.io/tools/icons.
+   */
+  icon: PropTypes.string,
+
   /**
-   * If `true`, the label is displayed as required and the input will be required.
+   * If `true`, the textfield will take up the full width of its container.
+   */
+  fullWidth: PropTypes.bool,
+
+  /**
+   * If `true`, the textfield will be disabled.
+   */
+  disabled: PropTypes.bool,
+
+  /**
+   * If `true`, the label is displayed as required.
    */
   required: PropTypes.bool,
 
   /**
+   * If `true`, the textfield will be displayed in an error state.
+   */
+  error: PropTypes.bool,
+
+  /**
+   * If `true`, the textfield will be focused during the first mount.
+   */
+  autoFocus: PropTypes.bool,
+
+  /**
+   * If `true`, a textarea element will be rendered instead of an textfield.
+   */
+  multiline: PropTypes.bool,
+
+  /**
    * Number of rows to display when multiline option is set to true.
    */
-  rows: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
+  rows: PropTypes.string,
 
   /**
-   * Maximum number of rows to display when multiline option is set to true.
+   * Focus event to use with UXPin interactions.
    */
-  rowsMax: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ]),
+  onFocus: PropTypes.func,
 
   /**
-   * Render a `Select` element while passing the `Input` element to `Select` as `input` parameter.
-   * If this option is set you must pass the options of the select as children.
+   * Blur event to use with UXPin interactions.
    */
-  select: PropTypes.bool,
-
-  SelectProps: PropTypes.object,
+  onBlur: PropTypes.func,
 
   /**
-   * Type attribute of the `Input` element. It should be a valid HTML5 input type.
+   * Change event to use with UXPin interactions.
    */
-  type: PropTypes.string,
+  onChange: PropTypes.func,
 
   /**
-   * The value of the `Input` element, required for a controlled component.
+   * PROPS NOT USED
    */
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-    PropTypes.bool,
-    PropTypes.arrayOf(
-      PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-        PropTypes.bool
-      ])
-    )
-  ]),
 
+  /**
+   * This property helps users to fill forms faster, especially on mobile devices.
+   * The name can be confusing, as it's more like an autofill.
+   * You can learn more about it here:
+   * https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill
+   * @uxpinignoreprop
+   */
+  autoComplete: PropTypes.string,
+
+  /**
+   * @uxpinignoreprop
+   */
+  children: PropTypes.node,
+
+  /**
+   * @uxpinignoreprop
+   */
+  className: PropTypes.string,
+
+  /**
+   * @uxpinignoreprop
+   */
+  FormHelperTextProps: PropTypes.object,
+
+  /**
+   * The id of the `input` element.
+   * Use that property to make `label` and `helperText` accessible for screen readers.
+   * @uxpinignoreprop
+   */
+  id: PropTypes.string,
+
+  /**
+   * @uxpinignoreprop
+   */
+  InputLabelProps: PropTypes.object,
+
+  /**
+   * Properties applied to the `Input` element.
+   * @uxpinignoreprop
+   */
+  InputProps: PropTypes.object,
+
+  /**
+   * Attributes applied to the native `input` element.
+   * @uxpinignoreprop
+   */
+  inputProps: PropTypes.object,
+
+  /**
+   * Use that property to pass a ref callback to the native input component.
+   * @uxpinignoreprop
+   */
+  inputRef: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
+
+  /**
+   * Name attribute of the `input` element.
+   * @uxpinignoreprop
+   */
+  name: PropTypes.string,
+
+  /**
+   * The variant to use.
+   */
+  /** @uxpinignoreprop */
   children: PropTypes.node,
 
   /**
    * The variant to use.
    */
   /** @uxpinignoreprop */
-  variant: PropTypes.oneOf(["standard", "outlined", "filled"])
+  variant: PropTypes.oneOf(["standard", "outlined", "filled"]),
+
+  /**
+   * The value of the `Input` element, required for a controlled component.
+   * @uxpinignoreprop
+   */
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
+    )
+  ]),
+
+  /**
+   * Maximum number of rows to display when multiline option is set to true.
+   * @uxpinignoreprop
+   */
+  rowsMax: PropTypes.string,
+
+  /**
+   * Render a `Select` element while passing the `Input` element to `Select` as `input` parameter.
+   * If this option is set you must pass the options of the select as children.
+   * @uxpinignoreprop
+   */
+  select: PropTypes.bool,
+  /**
+   * @uxpinignoreprop
+   */
+  SelectProps: PropTypes.object,
+
+  /**
+   * Type attribute of the `Input` element. It should be a valid HTML5 input type.
+   *  @uxpinignoreprop
+   */
+  type: PropTypes.string,
+
+    /**
+   * If `dense` or `normal`, will adjust vertical spacing of this and contained components.
+   * @uxpinignoreprop
+   */
+  margin: PropTypes.oneOf(["none", "dense", "normal"]),
+
 };
 
 export { TextField as default };
