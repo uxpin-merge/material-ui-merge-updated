69709a650c1c60cbdbf6c79d384854eb3aa11469
Jack Behar <jbehar@taylordigital.io>
2020-01-17 09:50:51 -0500

    Update

--- src/TabsWithPanel/TabsWithPanel.js
+++ src/TabsWithPanel/TabsWithPanel.js
@@ -19,7 +19,9 @@ function TabPanel(props) {
       aria-labelledby={`full-width-tab-${index}`}
       {...other}
     >
-      <Box paddingTop={3} paddingBottom={3} paddingLeft={3} paddingRight={3}>{children}</Box>
+      <Box paddingTop={3} paddingBottom={3} paddingLeft={3} paddingRight={3}>
+        {children}
+      </Box>
     </Typography>
   );
 }
@@ -39,23 +41,33 @@ function a11yProps(index) {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    borderBottom: '1px solid #e8e8e8',
-    
+    borderBottom: "1px solid #e8e8e8",
+    "& .MuiTab-wrapper": {
+      alignItems: "flex-start",
+      flexDirection: "row"
+    },
+    "& .MuiIcon-root": {
+      marginBottom: 0,
+      marginRight: theme.spacing(1)
+    },
+    "& .MuiTab-labelIcon": {
+      minHeight: "0",
+      paddingTop: "0"
+  }
   },
   tab: {
     fontSize: "0.95rem",
-    textTransform: 'none',
+    textTransform: "none",
     marginRight: theme.spacing(4),
     fontWeight: theme.typography.fontWeightRegular,
     minWidth: 100,
-    '&:hover': {
+    "&:hover": {
       // color: theme.palette.secondary.dark,
     },
-    '&.Mui-selected:hover': {
+    "&.Mui-selected:hover": {
       // color: "inherit",
-    },
+    }
   }
-  
 }));
 
 export default function TabsWithPanel(props) {
@@ -67,47 +79,41 @@ export default function TabsWithPanel(props) {
     setValue(newValue);
   }
 
-
   return (
-    <div >
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="full width tabs example"
-          {...props}
-          className={classes.root}
-        >
+    <div>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="full width tabs example"
+        {...props}
+        className={classes.root}
+      >
+        {props.tabs.map((item, index) => {
+          return (
+            <Tab
+              label={item.label}
+              key={index}
+              textColor="primary"
+              indicatorColor="primary"
+              {...(item.disabled && { disabled: true })}
+              {...(item.icon && { icon: <Icon> {item.icon} </Icon> })}
+              {...a11yProps(index)}
+              {...props}
+              className={classes.tab}
+            />
+          );
+        })}
+      </Tabs>
 
-            {props.tabs.map((item, index) => {
-                return (
-                    <Tab
-                        label={item.label}
-                        key={index}
-                        {...item.disabled && {'disabled' : true}}
-                        {...item.icon && {'icon' : <Icon> {item.icon} </Icon>}}
-                        {...a11yProps(index)}
-                        {...props}
-                        className={classes.tab}
-                    />
-                );
-            })}
-
-        </Tabs>
-      
-      {React.Children.map(
-        props.children,
-        (child, index) => (
-          <TabPanel value={value} index={index} dir={theme.direction}>
-            {React.cloneElement(child, { value: value })}
-          </TabPanel>
-        )
-      )}
-
+      {React.Children.map(props.children, (child, index) => (
+        <TabPanel value={value} index={index} dir={theme.direction}>
+          {React.cloneElement(child, { value: value })}
+        </TabPanel>
+      ))}
     </div>
   );
 }
 TabsWithPanel.propTypes = {
-
   /**
    * Array of tab labels and properties.
    * Use the following format:
@@ -125,40 +131,45 @@ TabsWithPanel.propTypes = {
       }
     ]
    */
-  tabs: PropTypes.arrayOf(PropTypes.shape(
-    {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
       label: PropTypes.string,
       disabled: PropTypes.bool,
-      icon: PropTypes.string,
-  })),
+      icon: PropTypes.string
+    })
+  ),
 
+  /**
+   * Number of the tab that supposed to be active. Starts with 0.
+   * @uxpinpropname  Active Tab
+   */
+  defaultValue: PropTypes.number,
 
   /**
    *  Determines additional display behavior of the Tabss:
-   *  - `scrollable` will invoke scrolling properties and allow for horizontally
-   *  scrolling (or swiping) of the Tabs bar.
-   *  -`fullWidth` will make the Tabss grow to use all the available space,
-   *  which should be used for small views, like on mobile.
+   *  - `scrollable` will display scroll arrows if tabs do not fit the container
+   *  - `fullWidth` will make the Tabs grow to use all the available space.
    *  - `standard` will render the default state.
    */
-  variant: PropTypes.oneOf(['standard', 'scrollable', 'fullWidth']),
+  variant: PropTypes.oneOf(["standard", "scrollable", "fullWidth"]),
 
   /**
    * The content of the component.
+   * @uxpinignoreprop
    */
-  /** @uxpinignoreprop */
   children: PropTypes.node,
 
   /**
    * Determines the color of the `Tabs`.
+   * @uxpinignoreprop
    */
-  textColor: PropTypes.oneOf(['secondary', 'primary', 'inherit']),
+  textColor: PropTypes.oneOf(["secondary", "primary", "inherit"]),
 
   /**
    * Determines the color of the indicator.
+   * @uxpinignoreprop
    */
-  indicatorColor: PropTypes.oneOf(['secondary', 'primary']),
-
+  indicatorColor: PropTypes.oneOf(["secondary", "primary"]),
 
   /**
    * Determine behavior of scroll buttons when Tabss are set to scroll
@@ -167,23 +178,14 @@ TabsWithPanel.propTypes = {
    * `off` will never present them
    */
   /** @uxpinignoreprop */
-  scrollButtons: PropTypes.oneOf(['auto', 'on', 'off']),
-
-
-  /**
-   * Number of the tab that supposed to be active initially. Starts with  0 for the first tab.
-   */
-  defaultValue: PropTypes.number,
-
-
-  
+  scrollButtons: PropTypes.oneOf(["auto", "on", "off"])
 };
 
 TabsWithPanel.defaultProps = {
-  tabs:[{label: "Tab 1222"}, {label: "Tab 2"}, {label: "Tab 3"}],
+  tabs: [{ label: "Tab 1222" }, { label: "Tab 2" }, { label: "Tab 3" }],
   defaultValue: 0,
   indicatorColor: "primary",
-  textColor:"primary",
-  variant:"standard",
-  scrollButtons:"auto"
+  textColor: "primary",
+  variant: "standard",
+  scrollButtons: "auto"
 };
