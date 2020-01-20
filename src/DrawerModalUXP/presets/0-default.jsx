62dd18042d5bf82c7fb1b86db15e972c9b61611d
Jack Behar <jbehar@taylordigital.io>
2020-01-20 11:27:05 -0500

    updates

--- src/DrawerModalUXP/presets/0-default.jsx
+++ src/DrawerModalUXP/presets/0-default.jsx
@@ -1,9 +1,63 @@
 import * as React from "react";
 
 import Drawer from "../DrawerModalUXP";
-
+import IglooSideNavigation from "../../IglooSideNavigation/IglooSideNavigation"
+var menus = 
+  [
+    {
+        "active": true,
+        "opened": true,
+        "label": "Marketing Services",
+        "submenu": [
+            {
+                "label": "Branding",
+                "submenu": [
+                    {
+                        "label": "Brochures"
+                    },
+                    {
+                        "label": "Business Cards"
+                    },
+                    {
+                        "label": "Logos"
+                    }
+                ]
+            },
+            {
+                "label": "Variable Printing",
+                "submenu": [
+                    {
+                        "label": "Igen"
+                    },
+                    {
+                        "label": "1-to-1"
+                    }
+                ]
+            },
+            {
+                "label": "Loyalty",
+                "active": true,
+            },
+            {
+                "label": "Social Media"
+            }
+        ]
+    },
+    {
+        "icon": "data_usage",
+        "label": "Data Solutions"
+    },
+    {
+        "icon": "widgets",
+        "label": "Technology"
+    },
+    {
+        "icon": "bookmarks",
+        "label": "From Side"
+    }
+]
 export default (
   <Drawer minHeight="600" anchor="left" open={true} variant="temporary" uxpId="1">
-    Drawer content
+    <IglooSideNavigation menus={menus} uxpId="2"/>
   </Drawer>
 );
