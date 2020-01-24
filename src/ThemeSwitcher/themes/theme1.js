e380ae6e100eeb548cddb942ad330dee939f52f0
Jack Behar <jbehar@taylordigital.io>
2020-01-24 16:22:16 -0500

    Updates

--- src/ThemeSwitcher/themes/theme1.js
+++ src/ThemeSwitcher/themes/theme1.js
@@ -1,14 +1,41 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import igloo from "./igloo"; 
+import igloo from "./igloo";
 
- const theme1 = createMuiTheme({
-   ...igloo,
-   palette: {
-      primary: {
-         main: "#2b2b2b",
+const theme1 = createMuiTheme({
+  ...igloo,
+  palette: {
+    primary: {
+      main: "#2b2b2b"
+    }
+  },
+  overrides: {
+    // Name of the component
+    MuiTabs: {
+      root: {
+        width: "100%",
+        borderRadius: "8px",
+        background: "linear-gradient(60deg, #ab47bc, #8e24aa)",
+        padding: 10,
+        boxShadow: "0px 3px 15px rgba(34, 35, 58, 0.5)"
+      },
+      indicator: {
+        height: "100%",
+        backgroundColor: "rgba(255, 255, 255, .2)",
+        borderRadius: "16px"
       }
-   }
+    },
+    MuiTab: {
+      root: {
+        textTransform: "initial",
+        margin: "0 16px",
+        minWidth: 0
+      },
+      wrapper: {
+        color: "#ffffff",
+        textTransform: "uppercase"
+      }
+    }
+  }
 });
 
 export default theme1;
-
--- src/ThemeSwitcher/themes/theme2.js
+++ src/ThemeSwitcher/themes/theme2.js
@@ -1,14 +1,62 @@
 import { createMuiTheme } from "@material-ui/core/styles";
-import igloo from "./igloo"; 
- 
- const theme2 = createMuiTheme({
-   ...igloo,
-   palette: {
-      primary: {
-         main: "#cccccc", //you can also use a color reference, i.e. blue[700]
+import igloo from "./igloo";
+
+const theme2 = createMuiTheme({
+  ...igloo,
+  palette: {
+    primary: {
+      main: "#cccccc"
+    }
+  },
+  overrides: {
+    // Name of the component
+    MuiTabs: {
+      root: {
+        width: "100%"
+      },
+      indicator: {
+        display: "none"
+      },
+      centered: {
+        alignItems: "center",
+        justifyContent: "center"
+      },
+    },
+    MuiTab: {
+      root: {
+        position: "relative",
+        display: "block",
+        borderRadius: "30px",
+        textAlign: "center",
+        transition: "all .5s",
+        padding: "10px 15px",
+        color: "#555555",
+        height: "auto",
+        opacity: "1",
+        margin: "10px 0",
+        float: "none",
+        "& + button": {
+          margin: "10px 0"
+        },
+        "&$selected": {
+          "&, &:hover": {
+            color: "#FFFFFF !important",
+            backgroundColor: "#00acc1",
+            boxShadow: "0 7px 10px -5px rgba(76, 175, 80, 0.4)"
+          }
+        }
+      },
+      wrapper: {
+        lineHeight: "24px",
+        textTransform: "uppercase",
+        fontSize: "12px",
+        fontWeight: "500",
+        position: "relative",
+        display: "block",
+        color: "inherit"
       }
-   }
+    }
+  }
 });
 
 export default theme2;
-
