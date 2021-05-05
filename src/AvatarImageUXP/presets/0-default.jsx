3e90d55e3d5ad6c72f0ef638311f0cc07c063a13
Jack Behar <jbehar@taylordigital.io>
2021-05-05 09:28:58 -0600

    Removed old avatars

--- src/AvatarImageUXP/presets/0-default.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import AvatarImageUXP from '../AvatarImageUXP';
-
-export default (
-  <AvatarImageUXP uxpId="1" src="http://i.pravatar.cc/">
-    M
-  </AvatarImageUXP>
-);
--- src/AvatarLetterUXP/presets/0-default.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import Avatar from '../AvatarLetterUXP';
-
-export default (
-  <Avatar uxpId="1">
-    JB
-  </Avatar>
-);
