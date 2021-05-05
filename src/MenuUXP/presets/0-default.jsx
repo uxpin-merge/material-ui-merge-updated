34a1573e1d6ac8087be70465a4d102b39428d889
Jack Behar <jbehar@taylordigital.io>
2021-05-05 11:32:48 -0600

    Clean-up

--- src/MenuUXP/presets/0-default.jsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-import Menu from '../MenuUXP';
-import MenuItem from "../../MenuItem/MenuItem";
-
-export default (
-  <Menu uxpId="1" />
-);
--- src/SliderUXP/presets/0-default.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-import Slider from "../SliderUXP";
-
-export default <Slider uxpId="1" defaultValue={80} />;
