e9e5877a61843e4c653d5f28bcaa6bc38571f757
Jack Behar <jack@uxpin.com>
2021-07-27 12:03:07 -0600

    Updates

--- src/CardMedia/CardMedia.js
+++ src/CardMedia/CardMedia.js
@@ -17,7 +17,6 @@ CardMedia.propTypes = {
    * Image to be displayed as a background image.
    * Either `image` or `src` prop must be specified.
    * Note that caller must specify height otherwise the image will not be visible.
-  * @uxpinignoreprop
    */
   image: PropTypes.string,
   /**
@@ -39,7 +38,7 @@ The height of the media
 CardMedia.defaultProps = {
   component: "img",
   height: 120,
-  src: "https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
+  image: "https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
 };
 
 export { CardMedia as default };
--- src/CardMedia/presets/0-default.jsx
+++ src/CardMedia/presets/0-default.jsx
@@ -4,6 +4,6 @@ import CardMedia from "../CardMedia";
 export default (
   <CardMedia uxpId="1"
     component="img"
-    src="https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
+    image="https://www.platingsandpairings.com/wp-content/uploads/2016/01/Paella-1-Square.jpg"
   />
 );
